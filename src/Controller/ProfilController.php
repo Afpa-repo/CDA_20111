35d513e6c67d1e9b4f56fb2a2cbc24b99f4c28f0
JulieDelacroixE <70313476+JulieDelacroixE@users.noreply.github.com>
2021-02-11 16:22:32 +0100

    changement mot de passe

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,7 +3,7 @@
 
 namespace App\Controller;
 
-
+use App\Repository\MembreRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +11,27 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\EditProfilType;
+use App\Entity\Membre;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfilController extends AbstractController
 {
+    /**
+     * @var MembreRepository
+     */
+    private $repository;
+
+    /**
+     * @param MembreRepository
+     * @param EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(MembreRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
     /**
      * @Route("/profil", name="profil")
      * @return Response
@@ -22,6 +40,7 @@ class ProfilController extends AbstractController
     {
         return $this->render('profil/view.html.twig');
     }
+
     /**
      * @Route("/profil/{id}/edit", name="profil_edit")
      * @param Request $request
@@ -30,16 +49,16 @@ class ProfilController extends AbstractController
      * @throws Exception
      */
     public function edit(Request $request,EntityManagerInterface $manager){
-
+        // On récupère l'utilisateur connecté
         $user = $this->getUser();
-
+        // Création du formulaire avec validation des valeurs par défaut si inchangées
         $formEditProfil =$this->createForm(EditProfilType::class, $user,
             ['validation_groups' => ['Default']
             ]);
 
         //analyse de la requete passée
         $formEditProfil->handleRequest($request);
-
+        //Vérification des données
         if ($formEditProfil->isSubmitted() && $formEditProfil->isValid()){
 
             $manager->persist($user);
@@ -47,10 +66,34 @@ class ProfilController extends AbstractController
 
             return $this -> redirectToRoute('profil');
         }
-        //Vérification des données passées pour le User
-        //dump($user);
+
         return $this-> render('profil/edit.html.twig',[
             'formProfil' => $formEditProfil->createView()
         ]);
     }
+
+    /**
+     * @Route("/profil/{id}/editpass", name="profil_edit_password")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return mixed
+     */
+    public function editPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
+        if($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+            $user = $this->getUser();
+
+            // mots de passe identiques ?
+            if($request->request->get('mdp') == $request->request->get('mdp2')){
+                $user->setMdp($passwordEncoder->encodePassword($user, $request->request->get('mdp2')));
+                $em->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour');
+                return $this->redirectToRoute('profil');
+            }else{
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques.');
+            }
+        }
+            return $this->render('profil/editPass.html.twig');
+    }
+
 }
